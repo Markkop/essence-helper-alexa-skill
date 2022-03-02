7e983256a1bb60944f3798f4121187659ed72cf4
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-03-02 10:04:44 -0300

    feat: language fallback

--- lambda/src/interceptors/LocalizationRequestInterceptor.ts
+++ lambda/src/interceptors/LocalizationRequestInterceptor.ts
@@ -2,6 +2,7 @@ import { RequestInterceptor } from 'ask-sdk-core';
 import * as sprintf from 'i18next-sprintf-postprocessor';
 import { use } from 'i18next';
 import { strings } from '../utilities/strings';
+import { LocaleTypes } from '../utilities/constants';
 
 /**
  * Adds translation functions to the RequestAttributes.
@@ -10,6 +11,7 @@ export const LocalizationRequestInterceptor: RequestInterceptor = {
   process(handlerInput) {
     use(sprintf).init({
       lng: handlerInput.requestEnvelope.request.locale,
+      fallbackLng: LocaleTypes.enUS,
       overloadTranslationOptionHandler:
         sprintf.overloadTranslationOptionHandler,
       resources: strings,
